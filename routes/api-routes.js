e45945fa7c6a1e1ef5f89123c932a930215da3b0
matt <you@example.com>
2020-03-17 21:53:52 -0700

    create new article routes is ready

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,13 +3,20 @@ const router = express.Router()
 const Article = require('../models/Article')
 
 router.get("/api",(req, res) =>{
+    res.send("<h1>Api Route</h1>")
+})
+
+router.get('/api/all', (res, req)=>{
     Article.find().then(res=> {
         res.send(res)
     }).catch(err=> console.log(err))
 })
 
-router.get('/api/all', (res, req)=>{
-
+router.post('/api/new', (req, res)=>{
+    Article.create({
+        title: req.body.title,
+        body: req.body.body
+    }).then(res=>{console.log(res)}).catch(err=>console.log(err));
 })
 
 module.exports = router;
