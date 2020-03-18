037b7555e4430d8b0a9b254ec9d335cc7e569216
matt <you@example.com>
2020-03-17 21:14:30 -0700

    succeful connection to mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 7000
+const mongoose = require("mongoose")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -9,3 +10,20 @@ app.use("/", require("./routes/api-routes"))
 app.listen(PORT, ()=> {
     console.log(`listening on http://localhost:${PORT}`)
 })
+
+mongoose.connect("mongodb+srv://ghost:catpaws@cluster0-h508y.mongodb.net/DinoDB?retryWrites=true&w=majority", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log("Error: ", err));
+
+
+
+
+
+
+
+
+
